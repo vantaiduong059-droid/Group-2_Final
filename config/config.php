e5ed05e154a23ad6c946b1d784182e2ea3434273
nguyendzai0sai <duongnguyen36bg@gmail.com>
2026-06-23 11:20:57 +0700

    fix: update BASE_URL to Group-2_Final_Student/public

--- config/config.php
+++ config/config.php
@@ -8,7 +8,7 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'attendance_system');
 
-define('BASE_URL', 'http://localhost/ins3064/final_project/public');
+define('BASE_URL', 'http://localhost/Group-2_Final_Student/public');
 
 // Bật thông báo lỗi trong quá trình phát triển
 ini_set('display_errors', 1);
